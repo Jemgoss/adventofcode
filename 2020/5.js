5895d46396575b0581d73c17615d1a4f222a5f2b
Jeremy Goss <jem@goss-family.net>
2020-12-05 12:56:23 -0500

    Day 5 js version.

---
+++ 2020/5.js
@@ -0,0 +1,25 @@
+const data = require('fs').readFileSync('5.input', 'utf8').trim().split("\n");
+
+// Part 1
+const decoder = (acc, key) => {
+  if (key === acc.upperKey) {
+    acc.seat += acc.upperSeatCount;
+  }
+  acc.upperSeatCount /= 2;
+  return acc;
+};
+
+const seatNumFn = (assignment) => {
+  return Array.from(assignment.slice(0, 7)).reduce(decoder, {upperKey: 'B', seat: 0, upperSeatCount: 64}).seat * 8 +
+         Array.from(assignment.slice(7)).reduce(decoder, {upperKey: 'R', seat: 0, upperSeatCount: 4}).seat;
+};
+
+const maxSeatNum = Math.max(...data.map(seatNumFn, data));
+console.log(maxSeatNum);
+
+// Part 2
+const seating = Array(maxSeatNum + 1);
+data.forEach(assignment => seating[seatNumFn(assignment)] = true);
+const firstOccupied = seating.findIndex(e => e !== undefined);
+const mySeat = seating.findIndex((e, i) => i > firstOccupied && e === undefined);
+console.log(mySeat);
