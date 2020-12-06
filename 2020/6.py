b834e8decfc954e15ec5b55ee0599a6e00f7cb74
Jeremy Goss <jem@goss-family.net>
2020-12-06 11:28:13 -0500

    Day 6

---
+++ 2020/6.py
@@ -0,0 +1,8 @@
+with open("6.input") as f:
+    data = f.read().rstrip('\n').split("\n\n")
+
+# Part 1
+print(sum([len(set(list(group.replace("\n", "")))) for group in data]))
+
+# Part2
+print(sum([len(set.intersection(*[set(list(g)) for g in group.split("\n")])) for group in data]))
