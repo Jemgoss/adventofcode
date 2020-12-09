607f0a26cea47e22a420eda56aeed241ad4cc46e
Jeremy Goss <jeremy.goss@hpe.com>
2020-12-09 13:42:08 -0500

    Day 9 js.

---
+++ 2020/9.js
@@ -0,0 +1,20 @@
+const data = require('fs').readFileSync('9.input', 'utf8').split("\n").map(x => parseInt(x));
+
+// Part 1
+let found_val = data.find((val, pos) => pos >= 25 && !data.slice(pos - 25, pos).find((a, i, arr) => arr.find((b, j) => j > i && a + b === val)));
+console.log(found_val);
+
+// Part 2
+for (let pos = 0; ; pos++) {
+  let sum = 0;
+  let end = pos;
+  while (sum < found_val) {
+    sum += data[end]
+    end++;
+  }
+  if (sum === found_val) {
+    let range = data.slice(pos, end);
+    console.log(Math.min(...range) + Math.max(...range));
+    break;
+  }
+}
