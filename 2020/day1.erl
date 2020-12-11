838a0f63a08393505b243a7eb9ecbfa232e27cca
Jeremy Goss <jem@goss-family.net>
2020-12-10 23:00:27 -0500

    Day 1 pure erlang.

--- 2020/day1.erl
+++ 2020/day1.erl
@@ -1,21 +1,38 @@
 -module(day1).
 -export([runparts/0]).
 
-comb(0, _) ->
-	[[]];
-comb(_, []) ->
-	[];
-comb(N, [H | T]) ->
-	[[H | L] || L <- comb(N-1, T)] ++ comb(N, T).
+part1([], _) ->
+	invalid;
+part1([_E | Tail], []) ->
+  part1(Tail, Tail);
+part1(Search = [E | _], [A | Rest]) ->
+  case E + A of
+    2020 ->
+      {E, A};
+    _ ->
+      part1(Search, Rest)
+  end.
+
+part2([], _,_) ->
+  invalid;
+part2([_E | Tail], [], Third) ->
+  part2(Tail, Tail, Third);
+part2(First, [_ | Tail], []) ->
+  part2(First, Tail, Tail);
+part2(Search = [E | _], Second = [A | _Rest], [B | BRest]) ->
+  case E + A + B of
+    2020 ->
+      {E, A, B};
+  	_ ->
+      part2(Search, Second, BRest)
+    end.
 
 runparts() ->
 	{ok, BinaryContents} = file:read_file("1.input"),
 	Data = [binary_to_integer(A) || A <- re:split(BinaryContents, <<"\n">>), A =/= <<>>],
 
-	% Part 1
-	{value, [A1, B1]} = lists:search(fun([A,B]) -> A + B =:= 2020 end, comb(2, Data)),
+	{A1, B1} = part1(Data, Data),
 	io:fwrite("~p * ~p = ~p~n", [A1, B1, A1 * B1]),
 
-	% Part 2
-	{value, [A2, B2, C2]} = lists:search(fun([A,B,C]) -> A + B + C =:= 2020 end, comb(3, Data)),
+	{A2, B2, C2} = part2(Data, Data, Data),
 	io:fwrite("~p * ~p * ~p = ~p~n", [A2, B2, C2, A2 * B2 * C2]).
