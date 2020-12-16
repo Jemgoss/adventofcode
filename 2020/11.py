fa6bc5ccfe4ec17f0bc242c7d8d83d93af4b92fa
Jeremy Goss <jem@goss-family.net>
2020-12-16 16:43:35 -0500

    Day 11 & 12.

---
+++ 2020/11.py
@@ -0,0 +1,77 @@
+with open("11.input") as f:
+    data = [l.rstrip("\n") for l in f.readlines()]
+
+# data = """L.LL.LL.LL
+# LLLLLLL.LL
+# L.L.L..L..
+# LLLL.LL.LL
+# L.LL.LL.LL
+# L.LLLLL.LL
+# ..L.L.....
+# LLLLLLLLLL
+# L.LLLLLL.L
+# L.LLLLL.LL""".split("\n")
+
+row_count = len(data)
+row_length = len(data[0])
+
+# Part 1
+def adjacent_seats(seating, row, col):
+    seats = 0
+    for r in range(max(row - 1, 0), min(row + 2, row_count)):
+        for c in range(max(col - 1, 0), min(col + 2, row_length)):
+            if r != row or c != col:
+                if seating[r][c] == '#':
+                    seats += 1
+    return seats
+
+seating = [list(r) for r in data]
+while True:
+    next_seating = [r.copy() for r in seating]
+
+    for r, row in enumerate(seating):
+        for c, seat in enumerate(row):
+            if seat != '.':
+                adjacent = adjacent_seats(seating, r, c)
+                if seat == 'L' and adjacent == 0:
+                    next_seating[r][c] = '#'
+                elif seat == '#' and adjacent >= 4:
+                    next_seating[r][c] = 'L'
+
+    if next_seating == seating:
+        break
+    seating = next_seating
+
+print(sum(r.count('#') for r in seating))
+
+# Part 2
+def visible_occupied(seating, row, col, dir):
+    while True:
+        row += dir[0]
+        col += dir[1]
+        if row < 0 or row == row_count or col < 0 or col == row_length:
+            return False
+        if seating[row][col] != '.':
+            break
+    return seating[row][col] == '#'
+
+seating = [list(r) for r in data]
+dirs = ((0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1), (-1,0), (-1,1))
+
+while True:
+    next_seating = [r.copy() for r in seating]
+
+    for r, row in enumerate(seating):
+        for c, seat in enumerate(row):
+            if seat != '.':
+                visible_seats = sum(visible_occupied(seating, r, c, dir) for dir in dirs)
+                if seat == 'L' and visible_seats == 0:
+                    next_seating[r][c] = '#'
+                elif seat == '#' and visible_seats >= 5:
+                    next_seating[r][c] = 'L'
+
+    if next_seating == seating:
+        break
+    seating = next_seating
+
+print(sum(r.count('#') for r in seating))
---
+++ 2020/12.py
@@ -0,0 +1,70 @@
+import math
+
+with open("12.input") as f:
+    data = f.readlines()
+
+# data = """F10
+# N3
+# F7
+# R90
+# F11""".split("\n")
+
+# Part 1
+dir = [1, 0]
+rad = 0
+pos = [0, 0]
+
+for line in data:
+    i = line[0]
+    val = int(line[1:])
+    #print(pos, dir, i, val)
+    if i == 'F':
+        pos[0] += dir[0] * val
+        pos[1] += dir[1] * val
+    elif i == 'N':
+        pos[1] += val
+    elif i == 'S':
+        pos[1] -= val
+    elif i == 'E':
+        pos[0] += val
+    elif i == 'W':
+        pos[0] -= val
+    else:
+        if i == 'L':
+            rad += math.radians(val)
+        elif i == 'R':
+            rad -= math.radians(val)
+        dir[0] = int(math.cos(rad))
+        dir[1] = int(math.sin(rad))
+print(pos, abs(pos[0]) + abs(pos[1]))
+
+# Part 2
+pos = [0, 0]
+way = [10, 1]
+
+for line in data:
+    i = line[0]
+    val = int(line[1:])
+    #print(pos, way, i, val)
+    if i == 'F':
+        pos[0] += way[0] * val
+        pos[1] += way[1] * val
+    elif i == 'N':
+        way[1] += val
+    elif i == 'S':
+        way[1] -= val
+    elif i == 'E':
+        way[0] += val
+    elif i == 'W':
+        way[0] -= val
+    else:
+        if i == 'L':
+            rad = math.radians(val)
+        elif i == 'R':
+            rad = math.radians(-val)
+        # rotation trans: (x * cos(T) - y * sin(T), x * sin(T) + y * cos(T))
+        x = way[0] * int(math.cos(rad)) - way[1] * int(math.sin(rad))
+        y = way[0] * int(math.sin(rad)) + way[1] * int(math.cos(rad))
+        way[0] = x
+        way[1] = y
+print(pos, abs(pos[0]) + abs(pos[1]))
